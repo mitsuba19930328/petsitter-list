9daa90dd0faedbd1d394d4e339ba1c0ab5ae869d
島崎塁 <mitsuba19930328@gmail.com>
2020-12-11 02:57:42 +0900

    user model symple validation created & form error message system added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,13 @@ class UsersController < ApplicationController
 
   def create
     # ストロングパラメーターを使用してユーザーインスタンス生成
-    user = User.new(user_params)
-    if user.save
+    @user = User.new(user_params)
+    if @user.save
       # ユーザー登録成功した場合
       redirect_to root_path, notice: "ユーザー登録が完了しました。"
     else
       # ユーザー登録失敗した場合
-      render new_user_path, notice: "ユーザー登録が失敗しました。"
+      render :new, notice: "ユーザー登録が失敗しました。"
     end
   end
 
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
     if user.update(user_params)
       redirect_to root_path, notice: "ユーザー情報を更新しました"
     else
-      render edit_user_path, notice: "ユーザー情報更新に失敗しました"
+      render :edit, notice: "ユーザー情報更新に失敗しました"
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,27 @@
 class User < ApplicationRecord
+  # 書く順番
+  # ①アソシエーション（belongs_toなど）
+  # ②gemなどの関数や他の定義
+  # ③AttributeMethodsモジュール
+  # ④attr関連
+  # ⑤バリデーション
+  # ⑥コールバック
+
+  # bcryptによるパスワード検証
   has_secure_password
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  # email用の正規表現
+  validates :name, { presence: true, length: { maximum: 30 }}
+  validate :validate_name_not_including_comma
+  validates :email, { presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
+
+  # email保存前に小文字に変換処理を行う
+  before_save { self.email = email.downcase }
+
+  private
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
+
 end
