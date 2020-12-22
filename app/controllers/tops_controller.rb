d362bbe70fa53eefce726d1e7c5c72d09f49ddee
島崎塁 <mitsuba19930328@gmail.com>
2020-12-22 23:51:22 +0900

    standard search system created by rensack

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,9 +2,21 @@ class TopsController < ApplicationController
   skip_before_action :login_required
 
   def index
+
+    # 画面表示用
     @user = current_user
-    @users = User.all
-    @users = User.page(params[:page]).per(5)
+    # @users = User.all
+    #
+    # 検索
+    @q = Petsitter.ransack(params[:q])
+    @petsitters = @q.result(distinct: true)
+
+    # ペットシッター一覧＋ページネーション用設定
+    @petsitters = Petsitter.page(params[:page]).per(5)
+    # @users = User.all.page(params[:page]).per(5)
+
+
+
   end
 
 end
