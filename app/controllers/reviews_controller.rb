fa763786463c7e08661702df4e1a5a7db85301a0
島崎塁 <mitsuba19930328@gmail.com>
2020-12-31 00:14:20 +0900

    review投稿、編集、削除機能を編集

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,9 +15,32 @@ class ReviewsController < ApplicationController
     end
   end
 
+  # reviewの編集
+  def edit
+    @review = Review.find(params[:id])
+    #
+    @petsitter = Petsitter.find(session[:petsitter_id])
+  end
+
+  # reviewの編集実行
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to petsitter_path(session[:petsitter_id])
+    else
+      flash.now[:alert] = 'レビュー情報更新に失敗しました。'
+      render petsitter_path(session[:petsitter_id])
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to root_path, notice: "コメントを削除しました。"
+  end
+
   private
     def review_params
       params.require(:review).permit(:title,:content,:rate,:petsitter_id)
     end
-
 end
