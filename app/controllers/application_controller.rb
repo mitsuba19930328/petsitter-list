cae145ca468286884471f1f1502f2f10b0125836
島崎塁 <mitsuba19930328@gmail.com>
2021-01-26 01:45:36 +0900

    カラムの追加&条件分岐の作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
-  before_action :login_required
+  helper_method :current_user,:user_signed_in?
+  before_action :login_required, :prepare_rensack
 
   private
     # ログイン中ユーザー取得メソッド
@@ -8,8 +8,23 @@ class ApplicationController < ActionController::Base
       @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 
+    # ユーザーがログインしているか判断するメソッド
+    def user_signed_in?
+      # current_userがあればログイン済みと判断
+      if current_user
+        return true
+      end
+      return false
+    end
+
     # 未ログインユーザーの場合は、アクション前にログイン画面に遷移
     def login_required
       redirect_to login_path unless current_user
     end
+
+    # rensack使用のための@q変数を事前に用意する
+    def prepare_rensack
+      @q = Petsitter.ransack(params[:q])
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,34 @@
 
 
 # 個別ユーザー
-Petsitter.create(name: "ニャンコシッター",email: "sitter1@example.com",address: "福岡県八女市黒木町黒木273-13", prefecture: "福岡県", town: "八女市黒木町黒木273-13",
-                 phone: "080-0001-0001", business_hours: "08:00~18:30", regular_holiday: "水曜日", pet_type: "犬、猫、うさぎ、他小動物",
+Petsitter.create(name: "ニャンコシッター",email: "sitter1@example.com",address: "福岡県八女市黒木町黒木273-13",
+                 prefecture: "福岡県", town: "八女市",block: "黒木町黒木273-13",apartment: "",area: "八女市",
+                 phone: "080-0001-0001", business_hours: "08:00~18:30", regular_holiday: "水曜日",
+                 pet_type: "犬、猫、うさぎ、他小動物",　overview:"犬猫ウサギなんでも得意なペットシッターです。明るく元気に営業中！",
                  qualification: "認定ペットシッター", registration_number: "000001", insurance: true, image: File.open("./public/uploads/seeds/img1.jpg"))
 
-Petsitter.create(name: "日向香織",email: "sitter2@example.com",address: "京都府京都市伏見区丹後町706-11", prefecture: "京都府", town: "京都市伏見区丹後町706-11",
-                 phone: "080-0002-0002", business_hours: "09:00~19:00", regular_holiday: "月曜日、金曜日", pet_type: "犬、猫",
+Petsitter.create(name: "日向香織",email: "sitter2@example.com",address: "京都府京都市伏見区丹後町706-11",
+                 prefecture: "京都府", town: "京都市",　block: "伏見区丹後町706-11",　apartment: "",　area: "京都市",
+                 phone: "080-0002-0002", business_hours: "09:00~19:00", regular_holiday: "月曜日、金曜日",
+                 pet_type: "犬、猫",　overview:"犬猫ウサギなんでも得意なペットシッターです。明るく元気に営業中！",
                  qualification: "認定ペットシッター", registration_number: "000002", insurance: true, image: File.open("./public/uploads/seeds/img2.jpg"))
 
-Petsitter.create(name: "いつでもどこでも",email: "sitter3@example.com",address: "東京都世田谷区南烏山3-138-12", prefecture: "東京都", town: "世田谷区南烏山3-138-12",
-                 phone: "080-0003-0003", business_hours: "10:00~20:00", regular_holiday: "火曜日", pet_type: "犬、猫、うさぎ、他小動物",
+Petsitter.create(name: "いつでもどこでも",email: "sitter3@example.com",address: "東京都世田谷区南烏山3-138-12",
+                 prefecture: "東京都", town: "世田谷区",　block: "南烏山3-138-12",　apartment: "世田谷ライオンズマンション201号室",　area: "世田谷区,白金,南青山",
+                 phone: "080-0003-0003", business_hours: "10:00~20:00", regular_holiday: "火曜日",
+                 pet_type: "犬、猫、うさぎ、他小動物",　overview:"犬猫ウサギなんでも得意なペットシッターです。明るく元気に営業中！",
                  qualification: "ペットシッター士", registration_number: "000003", insurance: true, image: File.open("./public/uploads/seeds/img3.jpg"))
 
-Petsitter.create(name: "愛知秀夫",email: "sitter4@example.com",address: "埼玉県狭山市狭山748-8", prefecture: "埼玉県", town: "狭山市狭山748-8",
-                 phone: "080-0004-0004", business_hours: "09:30~19:30", regular_holiday: "金曜日", pet_type: "猫、うさぎ",
+Petsitter.create(name: "愛知秀夫",email: "sitter4@example.com",address: "埼玉県狭山市狭山748-8",
+                 prefecture: "埼玉県", town: "狭山市",　block: "狭山748-8",　apartment: "狭山マンション021号室",　area: "狭山",
+                 phone: "080-0004-0004", business_hours: "09:30~19:30", regular_holiday: "金曜日",
+                 pet_type: "猫、うさぎ",　overview:"犬猫ウサギなんでも得意なペットシッターです。明るく元気に営業中！",
                  qualification: "認定ペットシッター", registration_number: "000004", insurance: true, image: File.open("./public/uploads/seeds/img4.jpg"))
 
-Petsitter.create(name: "御相模ゆうき",email: "sitter5@example.com",address: "愛知県豊田市城見町828-2", prefecture: "愛知県", town: "豊田市城見町828-2",
-                 phone: "080-0005-0005", business_hours: "08:30~18:30", regular_holiday: "木曜日", pet_type: "犬、猫、うさぎ",
+Petsitter.create(name: "御相模ゆうき",email: "sitter5@example.com",address: "愛知県豊田市城見町828-2",
+                 prefecture: "愛知県", town: "豊田市",　block: "城見町828-2",　apartment: "トヨタアパート101号室",　area: "豊田市,名古屋",
+                 phone: "080-0005-0005", business_hours: "08:30~18:30", regular_holiday: "木曜日",
+                 pet_type: "犬、猫、うさぎ",　overview:"犬猫ウサギなんでも得意なペットシッターです。明るく元気に営業中！",
                  qualification: "ペットシッター士", registration_number: "000005", insurance: true, image: File.open("./public/uploads/seeds/img5.png"))
 
 
@@ -27,7 +37,9 @@ Petsitter.create(name: "御相模ゆうき",email: "sitter5@example.com",address
 end
 
 25.times do |i|
-  Petsitter.create(name: "ワンニャンシッター #{i+5}",email: "sitter#{i*5}@example.com",address: "東京都稲城市東長沼771-1", prefecture: "東京都", town: "東京都稲城市東長沼771-1",
-                   phone: "080-000#{i*5}-000#{i*5}", business_hours: "10:00~19:30", regular_holiday: "月曜日", pet_type: "犬、猫",
+  Petsitter.create(name: "ワンニャンシッター #{i+5}",email: "sitter#{i*5}@example.com",address: "東京都稲城市東長沼771-1",
+                   prefecture: "東京都", town: "稲城市",　block: "東長沼771-1",　apartment: "",　area: "稲城市,多摩市",
+                   phone: "080-000#{i*5}-000#{i*5}", business_hours: "10:00~19:30", regular_holiday: "月曜日",
+                   pet_type: "犬、猫",　overview:"犬猫ウサギ可愛いよね！序盤中盤終盤隙なく営業中です",
                    qualification: "認定ペットシッター", registration_number: "00000#{i*5}", insurance: true, image: File.open("./public/uploads/seeds/default.jpg"))
 end
