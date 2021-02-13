14f8e65093d3ac09af1e067b37ef7ba57db1bd2f
島崎塁 <mitsuba19930328@gmail.com>
2021-02-13 20:32:37 +0900

    デザイン基本的な部分は一旦完成（全ページ）

---
+++ app/assets/javascripts/petsitter.js
@@ -0,0 +1,14 @@
+
+
+// $(document).ready(function() {
+//     // $("p").text("jQuery稼働テスト(稼働中)");
+//     // $('.selected-menu').addClass("border: none");
+//     // $('.selected-menu').addClsass("background: gray");
+//     $('.selected-menu').css({'background':'linear-gradient(to top, #f5f5f5, #FFF)','background':'gray'});
+//     // $('.selected-menu').addClass("border: none");
+// });
+
+
+// $(function(){
+//     $('#selected-menu').addClass('selected-menu');
+// });
