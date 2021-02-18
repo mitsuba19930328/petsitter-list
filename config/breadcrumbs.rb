68c3afaf2248c44e1a941ebb94cbb007bd500f6e
島崎塁 <mitsuba19930328@gmail.com>
2021-02-19 03:52:17 +0900

    パンクズリスト実装（デザイン未修正）&ペットシッターIndexHeader一旦作成

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,4 +1,93 @@
 
+crumb :root do
+  link "ペットシッターリスト", root_path
+end
+
+  crumb :petsitters do |petsitter|
+    link "全てのペットシッター", petsitters_path
+    parent :root
+  end
+
+    crumb :petsitter do |petsitter|
+      petsitter = Petsitter.find(params[:id])
+      link petsitter.name, petsitter_path(petsitter.id)
+      parent :petsitters
+    end
+
+crumb :user_edit do |user|
+  user = current_user
+  link "ユーザ情報編集", user_edit_path(user.id)
+  parent :user
+end
+
+  crumb :user do |user|
+    user = current_user
+    link "#{user.name}さん のマイページ", user_path(user.id)
+    parent :root
+  end
+
+    crumb :user_edit do |user|
+      user = current_user
+      link "ユーザ情報編集", user_edit_path(user.id)
+      parent :user
+    end
+
+    crumb :user_histories do |user|
+      user = current_user
+      link "行ったお店一覧", user_histories_path(user.id)
+      parent :user
+    end
+
+    crumb :user_clips do |user|
+      user = current_user
+      link "保存済み一覧", user_clips_path(user.id)
+      parent :user
+    end
+
+# crumb :archives do |archives,year|
+#   link "Archives", article_archive_path(archives)
+#   parent :articles
+# end
+
+# crumb :category do |category|
+#   link "Category"
+#   link category.cat_name, categories_path(category)
+#   parent :articles
+# end
+
+
+
+
+
+
+
+
+# crumb :petsitters do
+#   link "全てのペットシッター", petsitters_path
+#   parent :root
+# end
+#
+# # 子カテゴリーのパンくず
+# crumb :child_petsitter do |petsitter|
+#   petsitter = Petsitter.find(params[:id])
+#   # 表示しているページが子カテゴリーの一覧ページの場合
+#   if petsitter.has_children?
+#     link "#{petsitter.name}", petsitter_path(petsitter)
+#     parent :petsitters
+#
+#     # 表示しているページが孫カテゴリーの一覧ページの場合
+#   else
+#     link "#{petsitter.parent.name}", category_path(category.parent)
+#     parent :petsitters
+#   end
+# end
+
+# # 親カテゴリーのパンくず
+# crumb :petsitter_show do |petsitter|
+#   petsitter = Petsitter.find(params[:id])
+#   link "#{petsitter.name}", petsitter_path(petsitter.id)
+#   parent :petsitters
+# end
 
 # crumb :root do
 #   link "Home", root_path
