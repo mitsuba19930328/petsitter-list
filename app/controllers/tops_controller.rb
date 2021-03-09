21e01778644eef2541ccdf403b56f64e0aa46a92
島崎塁 <mitsuba19930328@gmail.com>
2021-03-10 01:32:31 +0900

    一部リファクタリングなど

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -4,24 +4,13 @@ class TopsController < ApplicationController
   def index
     # 画面表示用
     @user = current_user
-    # @users = User.all
-    #
-    # # 検索
-    # @q = Petsitter.ransack(params[:q])
-    # @petsitters = @q.result(distinct: true)
 
     # ペットシッター一覧＋ページネーション用設定
     @petsitters = Petsitter.page(params[:page]).per(5)
-    # @users = User.all.page(params[:page]).per(5)
-    #
+
     @all_petsitters = Petsitter.all
     @all_users = User.all
     @all_reviews = Review.all
-
-    # 以下はGoogleMapテスト
-    #  @test_petsitter = Petsitter.find(1)
-    # gon.address = @test_petsitter.address
-    #
 
     if current_user
       # likeされたペットシッターを@petsitters配列に入れていく
