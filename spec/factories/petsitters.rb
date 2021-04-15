afc6fa4ee6c92ca11530b9f8e7470d858d386b2d
島崎塁 <mitsuba19930328@gmail.com>
2021-04-16 00:32:36 +0900

    factory_botの追加とpetsitterモデルのテスト追加

--- spec/factories/petsitters.rb
+++ spec/factories/petsitters.rb
@@ -1,5 +1,24 @@
 FactoryBot.define do
   factory :petsitter do
+
+    name {"sample-petsitter"}
+    # sequence(:email) { |n| "petsitter#{n}@example.com"}
+    address {""}
+    prefecture {"東京都"}
+    town {"荒川区"}
+    phone {"090-0000-0000"}
+    business_hours {"10:00~19:00"}
+    regular_holiday {"年中無休"}
+    pet_type {"犬、猫、うさぎ"}
+    qualification {"有資格者多数"}
+    registration_number {"保管 20東京都保第000000号"}
+    insurance {true}
+    image {"tokyositter.jpg"}
+    apartment {"3階"}
+    block {"麻布十番一丁目1番1号"}
+    overview {"明るく元気に営業中"}
+    area {"六本木、麻布十番、白金高輪"}
+    url {"https://sample.com"}
     
   end
 end
--- spec/models/petsitter_spec.rb
+++ spec/models/petsitter_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Petsitter, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
+
+  context '新規登録' do
+
+    it "必要な情報が揃っていれば登録できる" do
+      expect(FactoryBot.create(:petsitter)).to be_valid
+    end
+
+    it "名前がなければ登録できない" do
+      expect(FactoryBot.build(:petsitter, name:'')).to_not be_valid
+    end
+
+    it "名前が重複していても登録できる" do
+      petsitter1 = FactoryBot.create(:petsitter, name: 'hogehoge')
+      expect(FactoryBot.build(:user, name: user1.name)).to be_valid
+    end
+
+  end
 end
